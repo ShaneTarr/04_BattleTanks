a185f08a5ba812f3000b289d0c538c16cec65761
SHANESPC\Shane <tarr18@hotmail.com>
2017-01-13 23:28:57 -0800

    BT23 Get the Player Controller with C++

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -13,10 +13,11 @@ UCLASS()
 class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
-public:
+
+private:
+	virtual void BeginPlay() override;
+
 	ATank* GetControlledTank() const;
 
-	virtual void BeginPlay() override;
-	
-	
+	ATank* GetPlayerTank() const;
 };
