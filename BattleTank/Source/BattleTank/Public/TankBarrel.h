32235c9cd3b690d6a25096a29918f36a2f15e5ae
SHANESPC\Shane <tarr18@hotmail.com>
2017-01-16 23:07:11 -0800

    BT43 Working Round Awkward Bygs

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -8,16 +8,16 @@
 /**
  * 
  */
-UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
-class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
+UCLASS(meta = (BlueprintSpawnableComponent)) //, hidecategories = ("Collision"))
+	class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
-	
+
 public:
 	// -1 is max downward speed, and +1 is max up movement
 	void Elevate(float RelativeSpeed);
-	
-private: 
+
+private:
 	UPROPERTY(EditAnywhere, Category = setup)
 		float MaxDegreesPerSecond = 10;
 
