4a6a26499867330a6ebed8cdab34147abb39a4a9
SHANESPC\Shane <tarr18@hotmail.com>
2017-01-23 22:34:24 -0800

    BT84 Preparing for Particles

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -25,4 +25,10 @@ public:
 private:
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* CollisionMesh = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UParticleSystemComponent* LaunchBlast = nullptr;
+
 };
